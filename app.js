be2ff8e95ddd10f9f85de4df8c5e3696e92b6e5a
Yanako Lajara <yanakolajara@pursuit.org>
2023-07-06 13:16:16 -0400

    Main setup

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
+const app = express()
+const controller = require('./controllers/resources.controller')
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req,res) => {
+    res.send('Hello')
+})
+
+app.get('*', (req,res) => {
+    res.status(404).send('Page not found')
+})
+
+module.exports = app
